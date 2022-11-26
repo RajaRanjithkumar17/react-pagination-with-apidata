d76ae939faf06d5e72de9f5a124366cbf0afa363
RajaRanjithkumar17 <rajaranjithkumar17.com>
2022-11-26 18:39:34 +0530

    final commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,35 +19,31 @@ const Home = () => {
 
   const [useapi, setapi] = useState([]);
   const [useapicurrent, setcurrentPage] = useState(1);
-  const [useapitodo, settodosPerPage] = useState(20);
+  const [perpages, settodosPerPage] = useState(10);
 
-  const indexOfLastTodo = useapicurrent * useapitodo;
-  const indexOfFirstTodo = indexOfLastTodo - useapitodo;
+  const indexOfLastTodo = useapicurrent * perpages;
+  const indexOfFirstTodo = indexOfLastTodo - perpages;
   const currentTodos = useapi.slice(indexOfFirstTodo, indexOfLastTodo);
 
   const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(useapi.length / useapitodo); i++) {
+  for (let i = 1; i <= Math.ceil(useapi.length / perpages); i++) {
     pageNumbers.push(i);
   }
 
   const fetchapiData = async () => {
-    
     const apiData = await axios.get("https://api.spacexdata.com/v3/launches");
     setapi(apiData?.data);
-   
   };
 
   useEffect(() => {
     fetchapiData();
   }, []);
 
-  // console.log(useapi);
+  console.log(useapi);
 
-  const handlePageChange=(pageNumbers)=>{
-    setcurrentPage(pageNumbers)
-
-   
-  }
+  const handlePageChange = (pageNumbers) => {
+    setcurrentPage(pageNumbers);
+  };
 
   return (
     <div className="rocakt_api">
@@ -56,37 +52,20 @@ const Home = () => {
       {currentTodos?.map((item, index) => {
         return (
           <div key={index} className="styl">
-            <Card sx={{ maxWidth: 345}}>
-        <CardHeader
-                  title={item.mission_name}
-          subheader={item.details}
-          
-        />
-        <CardMedia
-          component="img"
-          height="194"
-          image={item.links.mission_patch }
-          alt={item.launch_site.site_id}
-        />
-        <CardContent>
-        <Box sx={{ '& button': { m: 2 } }}>
-    
-        <Button variant="outlined" size="small"  href={item.links.wikipedia} >
-          wikipedia
-        </Button>
-        <Button variant="outlined" size="small" href={item.links.video_link}>
-          video_link 
-        </Button>
-       
-     
-      </Box>
-      </CardContent>
-     
-       
-      </Card>
-           
-          
-        </div>
+            <Card sx={{ maxWidth: 345 }} className="cardstyle">
+              <CardHeader title={item.mission_name} subheader={item.details} />
+              <CardMedia component="img"    height="194" image={item.links.mission_patch}  
+                alt={item.launch_site.site_id} />
+            
+              <CardContent>
+                <Box sx={{ "& button": { m: 2 } }}>
+                  <Button variant="outlined"   size="small"  
+                    href={item.links.wikipedia} > wikipedia</Button>
+                  <Button  variant="outlined" size="small"   href={item.links.video_link}>video_link</Button>
+                </Box>
+              </CardContent>
+            </Card>
+          </div>
         );
       })}
 
@@ -95,7 +74,7 @@ const Home = () => {
         return(
          <div key={index}>
            <Stack spacing={4}>
-          <Pagination count={item} color="secondary" onChange={()=>handlePageChange(item)} />
+          <Pagination count={item} color="primary" onChange={()=>handlePageChange(item)} />
         </Stack>
          </div>
         )
